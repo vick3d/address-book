14c30d4b608eded3aa80d8b22d28548ca32bf31b
vick3d <viktoro0090@gmail.com>
2019-01-29 13:46:49 +0100

    add/setup Event handling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log("Hello Students!");
+document.addEventListener('DOMContentLoaded', () => {
+    const addContactForm = document.querySelector('.new-contact-form')
+    
+    addContactForm.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const {
+            name,
+            email,
+            phone,
+            company,
+            notes,
+            twitter,
+        } = addContactForm.elements
+        
+        const contact = {
+            id: Date.now(),
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            company: company.value,
+            notes: notes.value,
+            twitter: twitter.value
+        }
+
+        console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+    })
+})
