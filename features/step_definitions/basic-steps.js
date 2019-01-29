d15490a2017f683abcae0f9f9ad9e1f7eb8d5d04
vick3d <viktoro0090@gmail.com>
2019-01-29 11:06:11 +0100

    add test steps structure

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,11 +1,25 @@
-const { Given, Then } = require("cucumber");
+const { Given, Then, When } = require('cucumber')
 
-Given("I visit the site", function() {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+Given('I visit the site', async function() {
+    return 'pending'
+})
 
-Then("I should see {string}", function(string) {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
+Then('I should see {string}', async function(content) {
+    return 'pending'
+})
+
+When('I click {string}', async function(string) {
+    return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string) {
+    return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+    return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+    return 'pending'
+})
