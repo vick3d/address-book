1e0881cd71f380e5545808b6fca379e16df168da
vick3d <viktoro0090@gmail.com>
2019-01-29 15:02:17 +0100

    add contact rendering script and persisting contacts. all green

--- features/support/world.js
+++ features/support/world.js
@@ -44,6 +44,13 @@ class AddressBookWorld {
         expect(actualCount).to.be.eq(expectedCount)
     }
 
+    async pageDoesNotHaveTextContent(unexpectedContent) {
+        const pageContent = await this.page.content()
+        let actualContent = pageContent.match(unexpectedContent)
+
+        expect(actualContent).to.be.eq(null)
+    }
+
     btnSelectorFromName(btnName) {
         switch (btnName) {
         case 'add contact':
@@ -57,6 +64,7 @@ class AddressBookWorld {
             break
         }
     }
+
 }
 
 setWorldConstructor(AddressBookWorld)
