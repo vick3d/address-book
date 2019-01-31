bfe20c754064dbefd873900d2809018dbca0387f
nevroje <joakim.berglind@gmail.com>
2019-01-31 09:44:04 +0100

    Added new contact card in app.js

--- src/app.js
+++ src/app.js
@@ -17,15 +17,25 @@ const renderContacts = () => {
             let li = document.createElement('li')
             li.className = 'list-reset'
             li.innerHTML = `
-            <div class="container bg-grey opacity-75 mb-4 card">
-                <div class="content">
-                    <p>Name: ${contact.name}<p>
-                    <p>Company: ${contact.company}<p>
-                    <p>Note: ${contact.notes}</p>
-                    Email: ${contact.email} | Twitter
-                    <a href="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a>
+            <div class="max-w md w-full lg:flex">
+             <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('')" title="Picture">
+            </div>
+              <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+               <div class="mb-8">
+                <p class="text-sm text-grey-dark flex items-center">
+                 Something
+                </p>
+                <div class="text-black font-bold text-xl mb-2"> something else</div>
+                <p class="text-grey-darker text-base">something something</p>
                 </div>
-            </div>    
+                <div class="flex items-center">
+                    <div class="text-sm">
+                        <p class="text-black leading-none"> name here?</p>
+                        <p class="text-grey-dark">something</p>
+                    </div>
+                </div>
+            </div>
+        </div>
          `
         ul.appendChild(li)                        
         })
